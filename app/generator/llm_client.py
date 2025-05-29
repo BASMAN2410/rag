af2f6c8d89d7d0d44e20a13771ef81767e2a9944
BAS <baskarsatheesh@gmail.com>
2025-05-28 23:38:06 -0400

    use run pod ollama url

--- app/generator/llm_client.py
+++ app/generator/llm_client.py
@@ -1,16 +1,23 @@
-import os
 import requests
+import os
 
-OLLAMA_URL = os.getenv("OLLAMA_API", "http://localhost:11434")
+OLLAMA_URL = os.getenv("OLLAMA_API", "https://tcfzj9r3teosn0-11434.proxy.runpod.net")
+MODEL_NAME = os.getenv("OLLAMA_MODEL", "llama3")
 print(f"Using OLLAMA API at {OLLAMA_URL}")
+
 def generate_response(prompt: str):
-    response = requests.post(
-        f"{OLLAMA_URL}/api/generate",
-        json={
-            "model": "llama3",
-            "prompt": prompt,
-            "stream": False
-        }
-    )
-    response.raise_for_status()
-    return response.json()["response"]
+    try:
+        response = requests.post(
+            f"{OLLAMA_URL}/api/generate",
+            json={
+                "model": MODEL_NAME,
+                "prompt": prompt,
+                "stream": False
+            },
+            timeout=20
+        )
+        response.raise_for_status()
+        return response.json()["response"]
+    except requests.RequestException as e:
+        print("Error while calling Ollama API:", e)
+        return "Error: Failed to get a response from Ollama."
